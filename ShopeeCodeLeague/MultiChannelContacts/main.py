384cd57639441991d7c917b813cdb9e398bb9b5a
YenChen <yenchen0416@gmail.com>
2021-08-29 22:38:10 +0800

    add: black format

--- ShopeeCodeLeague/MultiChannelContacts/main.py
+++ ShopeeCodeLeague/MultiChannelContacts/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 project_folder = os.path.dirname(__file__)
-filepath = os.path.join(project_folder, 'data/contacts.json')
+filepath = os.path.join(project_folder, "data/contacts.json")
 
 with open(filepath, "r") as f:
     json_data = json.load(f)
@@ -12,25 +12,28 @@ data = pd.DataFrame(json_data)
 
 # Union-Find Structrue
 class UF:
-  def __init__(self, node_list):
-    self.data = {nd: nd for nd in node_list}
+    def __init__(self, node_list):
+        self.data = {nd: nd for nd in node_list}
 
-  def _find_deepest_node(self, node):
-    if self.data[node] != node:
-      self.data[node] = self._find_deepest_node(self.data[node])
-      return self.data[node]
-    else:
-      return node
+    def _find_deepest_node(self, node):
+        if self.data[node] != node:
+            self.data[node] = self._find_deepest_node(self.data[node])
+            return self.data[node]
+        else:
+            return node
 
-  def connect(self, node_1, node_2):
-    if node_1 > node_2: node_1, node_2 = node_2, node_1
-    self.data[self._find_deepest_node(node_2)] = self._find_deepest_node(node_1)
+    def connect(self, node_1, node_2):
+        if node_1 > node_2:
+            node_1, node_2 = node_2, node_1
+        self.data[self._find_deepest_node(node_2)] = self._find_deepest_node(
+            node_1
+        )
 
-  def get_data(self):
-    return {k: self._find_deepest_node(v) for k, v in self.data.items()}
+    def get_data(self):
+        return {k: self._find_deepest_node(v) for k, v in self.data.items()}
 
 
-node_list = data['Id'].unique().tolist()
+node_list = data["Id"].unique().tolist()
 uf = UF(node_list)
 
 for col in ["OrderId", "Phone", "Email"]:
@@ -44,4 +47,4 @@ for col in ["OrderId", "Phone", "Email"]:
                 uf.connect(pivot_id, id_)
 
 dict_id_newid = uf.get_data()
-data['NewId'] = data["Id"].map(lambda x:dict_id_newid.get(x))
+data["NewId"] = data["Id"].map(lambda x: dict_id_newid.get(x))
